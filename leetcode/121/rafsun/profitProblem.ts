45e3eeb9827692193afbd6c6a2248df1a398375a
rafsun-jani-qp <rafsun.jani@question.com>
2024-12-19 17:36:21 +0600

    solve this problem

---
+++ leetcode/121/rafsun/profitProblem.ts
@@ -0,0 +1,18 @@
+var maximumProfit = function(prices) {
+    if (prices.length > 0) {
+        let minValue = prices[0];
+        let maxProfit = 0;
+        for (let i = 1; i < prices.length; i++) {
+            let profit = prices[i] - minValue;
+            if (profit > maxProfit) {
+                maxProfit = profit; 
+            }
+            if (prices[i] < minValue) {
+                minValue = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+    return 0;
+};
