60b593b40a70005d3d4dec0494c084ef1341942c
rafsun-jani-qp <rafsun.jani@question.com>
2024-12-30 13:24:58 +0600

    undo changes

--- leetcode/121/rafsun/profitProblem.ts
+++ leetcode/121/rafsun/profitProblem.ts
@@ -1,7 +1,7 @@
 var maximumProfit = function(prices) {
     if (prices.length > 0) {
         let minValue = prices[0];
-        let maxProfit = 1;
+        let maxProfit = 0;
         for (let i = 1; i < prices.length; i++) {
             let profit = prices[i] - minValue;
             if (profit > maxProfit) {
