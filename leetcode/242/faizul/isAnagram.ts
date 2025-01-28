e3177a41969ff6af9dcf96fd9db066ed82f248dc
Faizul Islam <faizul.islam@questionpro.com>
2025-01-28 09:19:57 +0600

    added solution of leetcode 242 valid anagram

---
+++ leetcode/242/faizul/isAnagram.ts
@@ -0,0 +1,13 @@
+function isAnagram(s: string, t: string): boolean {
+  if (s.length !== t.length) return false;
+
+  const map = new Map<string, number>();
+  for (let i = 0; i < s.length; i++) {
+    map.set(s[i], (map.get(s[i]) || 0) + 1);
+    map.set(t[i], (map.get(t[i]) || 0) - 1);
+  }
+  for (const count of map.values()) {
+    if (count !== 0) return false;
+  }
+  return true;
+}
