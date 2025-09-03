44dd83b2e27723029f090fada31ad731995ed66f
mahi160 <omarsifat288@gmail.com>
2025-09-03 14:24:46 +0600

    update

--- salauddin_sifat/1295/main.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        n = 0
-        for i in range(len(nums)):
-            
--- salauddin_sifat/88/main.js
+++ salauddin_sifat/88/main.js
@@ -5,5 +5,6 @@
  * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
  */
 let merge = function (nums1, m, nums2, n) {
-  for (let i = m; i < m + n; i++) {}
+  for (let i = 0; i < m + n; i++) {}
+  return nums1;
 };
