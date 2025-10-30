89a006b4966545c7f7bfbd1c836dbe1323376ef5
Samsul Alam <samsul@Samsuls-MacBook-Air.local>
2025-10-30 11:48:53 +0600

    13 challenge roman to integer

---
+++ samsul_alam/oct25/13/13.js
@@ -0,0 +1,8 @@
+function romanToInt(s) {
+  const map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  let total = 0;
+  for (let i = 0; i < s.length; i++) {
+    total += map[s[i]] < map[s[i + 1]] ? -map[s[i]] : map[s[i]];
+  }
+  return total;
+}
