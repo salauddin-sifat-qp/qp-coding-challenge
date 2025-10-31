00a2539f818be457d66f19556026b03ad4262d89
khairul-anik-qp <khairul.anik@questionpro.com>
2025-10-31 10:57:59 +0600

    solve leetcode 13

---
+++ khairul_anik/oct25/13/index.js
@@ -0,0 +1,26 @@
+function romanToInt(s) {
+    const romans = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+    let total = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let current = romans[s[i]];
+        let next = romans[s[i + 1]];
+
+        if (next && current < next) {
+            total -= current;
+        } else {
+            total += current;
+        }
+    }
+
+    return total;
+};
