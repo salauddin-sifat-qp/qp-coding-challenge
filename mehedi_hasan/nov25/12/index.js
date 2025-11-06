37626015bf3184bd42f76a4177762aeb42382b7a
Mehedi Hasan <mehedi.hasan@questionpro.com>
2025-11-06 16:27:51 +0600

    Solved the problem 12 Integer to Roman

---
+++ mehedi_hasan/nov25/12/index.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+
+const roman = new Map([
+  ["M", 1000],
+  ["CM", 900],
+  ["D", 500],
+  ["CD", 400],
+  ["C", 100],
+  ["XC", 90],
+  ["L", 50],
+  ["XL", 40],
+  ["X", 10],
+  ["IX", 9],
+  ["V", 5],
+  ["IV", 4],
+  ["I", 1],
+]);
+
+var intToRoman = function (num) {
+  let ans = "";
+  let temp = num;
+  while (temp != 0) {
+    roman.forEach((value, key) => {
+      if (value <= temp) {
+        let res = Math.floor(temp / value);
+        temp = temp % (res * value);
+        console.log(res * value, temp, key.repeat(res));
+
+        ans += key.repeat(res);
+        return;
+      }
+    });
+  }
+  return ans;
+};
+const number = 41;
+console.log(intToRoman(number));
