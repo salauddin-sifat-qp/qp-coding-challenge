19a0aabe6e64ff6f3b434664816e83a006de7351
Mehedi Hasan <mehedi.hasan@questionpro.com>
2025-11-28 11:28:18 +0600

    Add a second solution to for Valid Palindrome II

--- mehedi_hasan/nov25/680/index.js
+++ mehedi_hasan/nov25/680/index.js
@@ -11,6 +11,7 @@ const testcases = ["aba", "abca", "abc"];
 
 const expected = [true, true, false];
 
+// Solution 1
 const isValidPalindrome = function (s, leftIndex, rightIndex) {
   while (leftIndex < rightIndex) {
     if (s[leftIndex] != s[rightIndex]) {
@@ -22,6 +23,7 @@ const isValidPalindrome = function (s, leftIndex, rightIndex) {
   return true;
 };
 
+// 5ms
 var validPalindrome = function (s) {
   let left = 0;
   let right = s.length - 1;
@@ -40,4 +42,34 @@ var validPalindrome = function (s) {
   return true;
 };
 
-console.log(validPalindrome("ab bba"));
+// 4ms
+// solution 2
+var validPalindrome2 = function (s) {
+  let trySkipping = (side) => {
+    let left = 0;
+    let right = s.length - 1;
+    let isSkipped = false;
+
+    while (left < right) {
+      if (s[left] != s[right] && !isSkipped) {
+        if (side === "l") {
+          left++;
+        } else {
+          right--;
+        }
+
+        isSkipped = true;
+
+        continue;
+      } else if (s[left] != s[right]) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+    return true;
+  };
+  return trySkipping("l") || trySkipping("r");
+};
+
+console.log(validPalindrome2("abca"));
