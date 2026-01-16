bef963851c57bd4d8625d1b6100ad2b9b3c51884
Fahim Hasnat <37208911+fahimHasnat@users.noreply.github.com>
2026-01-16 11:35:37 +0600

    153_find_min_in_rotated_sorted_array.ts

---
+++ fahim_hasnat/jan26/153. Find Minimum in Rotated Sorted Array/153_find_min_in_rotated_sorted_array.ts
@@ -0,0 +1,17 @@
+// Submission URL: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/submissions/1886462751
+function findMin(nums: number[]): number {
+    let left = 0
+    let right = nums.length - 1
+
+    while (left < right){
+        let mid = Math.floor((left + right)/2)
+
+        if (nums[mid] > nums[right]){
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+
+    return nums[left]
+};
